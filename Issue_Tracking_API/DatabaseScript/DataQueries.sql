02f84498da2944bb2fd67e1786ad9c517a416397
Anshu <anshurai98@gmail.com>
2020-04-16 20:01:18 +1200

    Add project files.

---
+++ Issue_Tracking_API/DatabaseScript/DataQueries.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[IssueDetails] ON
+INSERT INTO [dbo].[IssueDetails] ([Id], [Issue_Description], [Issue_Reporter_Name], [Issue_Reported_Date], [Issue_Assignee_Name]) VALUES (1, N'Monitor is not working', N'Jagdeep Singh', N'2020-04-16 12:00:00', N'James Hong')
+INSERT INTO [dbo].[IssueDetails] ([Id], [Issue_Description], [Issue_Reporter_Name], [Issue_Reported_Date], [Issue_Assignee_Name]) VALUES (2, N'Laptop hard disk crashed', N'Lessa li', N'2020-04-15 14:02:00', N'Amr Adel')
+SET IDENTITY_INSERT [dbo].[IssueDetails] OFF
